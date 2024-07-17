785b606865c27126665d8e16c152786899f0eb3b
fadywadea <fwadea3@gmail.com>
2024-07-17 08:40:48 +0300

    Add in updateCategoryVal => array of tasks

--- src/modules/category/category.validation.js
+++ src/modules/category/category.validation.js
@@ -4,6 +4,7 @@ import Joi from "joi";
 
 const addCategoryVal = Joi.object({
   name: Joi.string().min(2).max(100).required().trim(),
+  tasks: Joi.array().items(Joi.string().hex().length(24)).required(),
 });
 
 const paramsIdVal = Joi.object({
@@ -12,6 +13,7 @@ const paramsIdVal = Joi.object({
 
 const updateCategoryVal = Joi.object({
   name: Joi.string().min(2).max(100).trim(),
+  tasks: Joi.array().items(Joi.string().hex().length(24)),
   id: Joi.string().hex().length(24).required(),
 });
 
