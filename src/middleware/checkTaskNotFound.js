ab935e54862ecb96ef2fd6bd5d863a34b4f12604
fadywadea <fwadea3@gmail.com>
2024-07-17 08:36:37 +0300

    Remove: comment and console.log

--- src/middleware/checkTaskNotFound.js
+++ src/middleware/checkTaskNotFound.js
@@ -7,8 +7,6 @@ export const checkTaskNotFound = catchError(async (req, res, next) => {
   try {
     const { tasks } = req.body;
     const task = await taskModel.find({ _id: { $in: tasks } })
-      // .toArray(function (err, docs) { console.log(docs); res.send(docs) });
-    console.log(task);
     next();
   } catch (e) {
     res.status(500).json({ error: `Error in server: ${e}` });
